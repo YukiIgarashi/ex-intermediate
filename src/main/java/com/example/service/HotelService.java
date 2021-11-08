e5dcd5efc50b18e3d7c8cb9f4fe475c3fb8a7a02
YukiIgarashi <yuki.igarashi@rakus-partners.co.jp>
2021-11-08 17:20:06 +0900

    演習2サービスクラス作成

---
+++ src/main/java/com/example/service/HotelService.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Hotel;
+import com.example.repository.HotelRepository;
+
+/**
+ * 
+ * @author igayuki
+ *
+ */
+@Service
+public class HotelService {
+	
+	@Autowired
+	HotelRepository repository;
+	
+	public List<Hotel> findByPrice(Integer price)  {
+		
+		return repository.findByPrice(price);
+		
+	}
+
+}
