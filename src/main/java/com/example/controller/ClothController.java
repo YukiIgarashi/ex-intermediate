be625ac800517fb87d76a2e605f0668880cde20f
YukiIgarashi <yuki.igarashi@rakus-partners.co.jp>
2021-11-08 23:50:59 +0900

    演習3コントローラークラス作成（初期画面遷移用メソッド追加）

---
+++ src/main/java/com/example/controller/ClothController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.ClothService;
+
+@Controller
+@RequestMapping("/cloth")
+public class ClothController {
+	
+	@Autowired
+	ClothService service;
+	
+	@RequestMapping("")
+	public String search(Model model) {
+		
+		List<String> colorList = new ArrayList<>();
+		
+		colorList.add("赤");
+		colorList.add("青");
+		colorList.add("白");
+		colorList.add("黄");
+		
+		model.addAttribute("colorList", colorList);
+		
+		return "search";
+		
+	}
+	
+	
+
+}
